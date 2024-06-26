410a2581456cbefa9921f67845f08e1cf8b7d00e
Alex <alexjuniorwisper@gmail.com>
2024-06-26 12:11:00 +0100

    Initial Commit

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -6,7 +6,7 @@ def main():
         number = int(input("Enter a number to see its multiplication table: "))
         
         # Generate and print the multiplication table using a for loop
-        for i in range(1, 10+1):
+        for i in range(1, 11):
             product = number * i
             print(f"{number} * {i} = {product}")
     
@@ -16,4 +16,5 @@ def main():
 # Ensures that the main function runs when the script is executed
 if __name__ == "__main__":
     main()
+
 
